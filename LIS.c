ceede239f65c46cc00ecc20be4731e257698ed57
Tommaso <tommy.terribili@gmail.com>
2022-07-21 17:18:51 +0200

    quick push

--- LIS.c
+++ LIS.c
@@ -1,56 +1,28 @@
 #include "push_swap.h"
 
-int	find_longest(int **matrix, int mat_x, int mat_y)
+int	*hash_init(int *arr, int len)
 {
-	int	cont;
-	int	max;
-	int	index;
 	int	i;
-	int	j;
 
-	i = 0;
-	max = 0;
-	while (i < )
+	arr = (int *)malloc(sizeof(int *) * len);
+	if (!arr)
 	{
-		cont = 0;
-		j = 0;
-		while ()
-		{
-			/* code */
-		}
-		if (cont > max)
-		{
-			index = i;
-		}
+		printf("error while allocating hash");
+		exit(0);
 	}
-
+	i = 0;
+	while (i < len)
+	{
+		arr[i] = i;
+		i++;
+	}
+	return (arr);
 }
 
 void LIS(t_stack *stacks)
 {
-	int	**m;
-	int	i; // x index
-	int	j; // y iterator
-	int	k; // y index
-	int	res;
+	int *hash;
 
-	j = 0;
-	while (i < stacks->len_lis)
-	{
-		k = 1;
-		i = 0;
-		m[i][j] = stacks->lis[i];
-		while (j < stacks->len_lis)
-		{
-			if (stacks->lis[j] > stacks->lis[i])
-			{
-				m[i][k] = stacks->lis[j];
-				k++;
-			}
-			j++;
-		}
-		i++;
-	}
-	res = find_longest(m, ft_calc_);
-
+	hash = hash_init(hash, stacks->len_lis);
+	
 }
--- algo.c
+++ algo.c
@@ -12,70 +12,44 @@
 
 #include "push_swap.h"
 
-int	*flag_flush(int *arr, int len, int flag)
+// stack_a |  50 |  3 | 10 | 7 | 40 |  80|
+// purolis |  4  |  4 | 3  | 3 |  2 |  1 |
+// -------------------------------
+// flagged |  0  |  1 | 0  | 1 |  1 |  1 |
+// |
+// 4 4 3 3 2 	1
+// 50 3 10 7 40 80
+void	flag_manager(t_stack *stacks)
 {
-	int		i;
-	bool	first;
+	int	i;
+	int	j;
+	int *arr;
 
-	i = len;
-	first = true;
-	while (--i > 0)
-	{
-		// if (first && arr[i] == flag)
-		// {
-		// 	first = false;
-		// 	continue ;
-		// }
-		// else if (arr[i] == flag)
-		// {
-			if (arr[i] == flag)
-				arr[i] = 1;
-			else
-				arr[i] = 0;
-		// }
-	}
-	printf("----flagged flush----\n");
-	i = -1;
-	while (++i < len)
-		printf("%d\n", arr[i]);
-	return (arr);
-}
-
-void	lis_manager(int *lis, int *slave, int len, int max)
-{
-	int i;
-	int first;
-	int k;
-
-
-	first = 1;
-	i = len;
-	printf("max: %d\n", max);
+	arr = allocation_helper(stacks->len_a, 1);
+	i = stacks->len_lis;
 	while (--i > -1)
 	{
-		if (slave[i] == max && first == 1)
-		{
-			lis[i] = 1;
-			first = 0;
-			continue ;
-		}
-		else if (first == 0)
-		{
-			lis[i] = 0;
-		}
+		j = i + 1;
+		if (stacks->stack_a[i] < stacks->stack_a[j] && stacks->lis[i] > stacks->lis[j])
+			arr[i] = 1;
+		else
+			arr[i] = 0;
+		// else if (stacks->stack_a[i] > stacks->stack_a[j] ||stacks->lis[i] < stacks->lis[j] )
 	}
-	printf("----cane----\n");
-	k = -1;
-	while (++k < len)
-		printf("%d\n", lis[k]);
+	if (stacks->stack_a[stacks->len_lis - 2] < stacks->stack_a[stacks->len_lis - 1]
+		&& stacks->lis[stacks->len_lis - 2] > stacks->lis[stacks->len_lis - 1])
+		arr[stacks->len_lis - 1] = 1;
+	i = -1;
+	printf("---porcaccio il signore---\n");
+	while (++i < stacks->len_lis)
+		printf("%d\n", arr[i]);
 }
 
-int	_lis(int *arr, int len, int *lis, int *slave)
+int	_lis(int *arr, int len, int *lis)
 {
 	int	max;
 	int	i;
 	int	j;
-	int k = 0;
 
 	i = len;
 	max = 0;
@@ -86,73 +60,18 @@ int	_lis(int *arr, int len, int *lis, int *slave)
 		{
 			if (arr[i] < arr[j])
 			{
-				max = find_max(slave, i);
-				if (max > slave[j])
-					slave[i] = max;
+				max = find_max(lis, i);
+				if (max > lis[j])
+					lis[i] = max;
 				else
-					slave[i] = 1 + slave[j];
+					lis[i] = 1 + lis[j];
 			}
 			j++;
 		}
-		printf("----dio----\n");
-		k = -1;
-		max = find_max(slave, i);
-		while (++k < len)
-			printf("%d\n", slave[k]);
-		lis_manager(lis, slave, len, max);
 	}
 	max = find_max(lis, i);
-	return (max);
+	return(max);
 }
-
-
-// int _lis(int *arr, int len, int *lis)
-// {
-// 	int	max;
-// 	int	i;
-// 	int	j;
-// 	int	curr_max;
-// 	int	*slave;
-// 	// int	*master;
-
-// 	slave = (int *)malloc(sizeof(int *) * len);
-
-// 	if (!(slave))
-// 	{
-// 		printf("error while allocating master and slave\n");
-// 		exit(0);
-// 	}
-// 	i = len;
-// 	while (--i > -1)
-// 	{
-// 		j = i + 1;
-// 		while (j < len)
-// 		{
-// 			if (arr[i] < arr[j])
-// 			{
-// 				curr_max = find_max(slave, i);
-// 				if (curr_max > slave[j])
-// 				{
-// 					// lis[i] = max;
-// 					slave[i] = curr_max;
-// 				}
-// 				else
-// 				{
-// 					// lis[i] = 1 + lis[j];
-// 					slave[i] = 1 + slave[j];
-// 				}
-// 			}
-// 			j++;
-// 		}
-// 		// if (curr_max > max)
-// 		// {
-// 		// 	max = curr_max;
-// 		// 	ft_memcpy(lis, slave, len);
-// 		// }
-// 	}
-// 	max = find_max(lis, i);
-// 	return (max);
-// }
 
 int	lis(t_stack *stacks)
 {
@@ -160,14 +79,14 @@ int	lis(t_stack *stacks)
 	int	i;
 
 	printf("len_lis: %d\n", stacks->len_lis);
-	stacks->slave = allocation_helper(stacks->len_lis, 1);
-
-	max = _lis(stacks->stack_a, stacks->len_a, stacks->lis, stacks->slave);
+	// stacks->slave = allocation_helper(stacks->len_lis, 1);
+	max = _lis(stacks->stack_a, stacks->len_a, stacks->lis);
 	// printf("flagged[0]: %d\n", flagged[0]);
 	printf("----slave----\n");
 	i = -1;
 	while (++i < stacks->len_lis)
-		printf("%d\n", stacks->slave[i]);
-	// flag_manager(stacks);
+		printf("%d\n", stacks->lis[i]);
+	write(1, "a\n", 2);
+	flag_manager(stacks);
 	return (max);
 }
