2542161290b97751bafab8d47bb50dd95365790e
Tommaso <tommy.terribili@gmail.com>
2022-07-21 18:52:03 +0200

    finalmente questo dio cane di lis funziona (o almeno spero)

--- algo.c
+++ algo.c
@@ -12,33 +12,48 @@
 
 #include "push_swap.h"
 
-// stack_a |  50 |  3 | 10 | 7 | 40 |  80|
-// purolis |  4  |  4 | 3  | 3 |  2 |  1 |
+// stack_a |  50 |  3 | 10 | 7 | 40 |  80| 27 |  1|
+// purolis |  4  |  4 | 3  | 3 |  2 |  1 |  1 |  1|
 // -------------------------------
 // flagged |  0  |  1 | 0  | 1 |  1 |  1 |
 // |
 // 4 4 3 3 2 	1
 // 50 3 10 7 40 80
+// 1 expected in indexes: 1, 3, 4, 5
+
+
+
 void	flag_manager(t_stack *stacks)
 {
 	int	i;
 	int	j;
+	int prev;
 	int *arr;
 
-	arr = allocation_helper(stacks->len_a, 1);
-	i = stacks->len_lis;
-	while (--i > -1)
+	arr = allocation_helper(stacks->len_a, 0);
+	i = 0;
+	prev = 0;
+	while (i < stacks->len_lis)
 	{
 		j = i + 1;
 		if (stacks->stack_a[i] < stacks->stack_a[j] && stacks->lis[i] > stacks->lis[j])
+		{
 			arr[i] = 1;
+			prev = i;
+		}
 		else
-			arr[i] = 0;
-		// else if (stacks->stack_a[i] > stacks->stack_a[j] ||stacks->lis[i] < stacks->lis[j] )
+		{
+			if (stacks->lis[i] == 1 && stacks->lis[j] == 1 && stacks->stack_a[i] > stacks->stack_a[j]
+				&& stacks->stack_a[i] > stacks->stack_a[prev])
+			{
+				arr[i] = 1;
+				prev = i;
+			}
+			else
+				arr[i] = 0;
+		}
+		i++;
 	}
-	if (stacks->stack_a[stacks->len_lis - 2] < stacks->stack_a[stacks->len_lis - 1]
-		&& stacks->lis[stacks->len_lis - 2] > stacks->lis[stacks->len_lis - 1])
-		arr[stacks->len_lis - 1] = 1;
 	i = -1;
 	printf("---porcaccio il signore---\n");
 	while (++i < stacks->len_lis)
@@ -90,3 +105,36 @@ int	lis(t_stack *stacks)
 	flag_manager(stacks);
 	return (max);
 }
+
+// shitty condition
+// if (stacks->lis[i] > stacks->lis[j])
+// {
+// 	printf("comparing %d (stack_a[%d]) with  %d (stack_a[%d])\n", stacks->stack_a[i], i,stacks->stack_a[j], j);
+// 	printf("comparing %d (lis[%d]) with  %d (lis[%d])\n", stacks->lis[i], i, stacks->lis[j], j);
+// 	if (stacks->stack_a[i] < stacks->stack_a[j])
+// 	{
+// 		arr[i] = 1;
+// 		printf("value 1 has been assigned to arr[%d]\n", i);
+// 	}
+// 	else
+// 	{
+// 		arr[i] = 0;
+// 		printf("value 0 has been assigned to arr[%d]\n", i);
+// 	}
+// }
+// else if ((stacks->lis[i] && stacks->lis[j]) == 1)
+// {
+// 	printf("comparing %d (stack_a[%d]) with  %d (stack_a[%d])\n", stacks->stack_a[i], i,stacks->stack_a[j], j);
+// 	printf("comparing %d (lis[%d]) with  %d (lis[%d])\n", stacks->lis[i], i, stacks->lis[j], j);
+// 	write(1, "a\n", 2);
+// 	if (stacks->stack_a[i] < stacks->stack_a[j])
+// 	{
+// 		arr[i] = 1;
+// 		printf("value 1 has been assigned to arr[%d]\n", i);
+// 	}
+// 	else
+// 	{
+// 		arr[i] = 0;
+// 		printf("value 0 has been assigned to arr[%d]\n", i);
+// 	}
+// }
